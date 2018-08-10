41c7fa687d41d9504d4f587afa8311696c03ba8a
sweezy793 <sarthaks793@gmail.com>
2018-08-10 16:36:52 +0530

    layout totally changed

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ const ipc=require('electron').ipcMain
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 1000, height: 700})
   
     // and load the index.html of the app.
     win.loadFile('src/index.html')
   
     // Open the DevTools.
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
   
     // Emitted when the window is closed.
     win.on('closed', () => {
